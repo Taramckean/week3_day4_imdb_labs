dfc30eddba4699d52f0bb2286951840dd5114c65
tara <tara.mck@hotmail.com>
2018-05-31 14:34:39 +0100

    first commit

---
+++ console.rb
@@ -0,0 +1,17 @@
+require_relative( 'models/star' )
+require_relative( 'models/movie' )
+require_relative( 'models/casting' )
+
+require('pry')
+
+Star.delete_all()
+Movie.delete_all()
+Casting.delete_all()
+
+
+
+
+
+
+binding.pry
+nil
